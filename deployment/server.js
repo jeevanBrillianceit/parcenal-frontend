12a0c703c37cfd74bd42c2321f7c8509519b966e
Jeevan Singh <jeevan.singh@watermelon.us>
2025-08-08 09:18:58 +0530

    code updated

--- deployment/server.js
+++ deployment/server.js
@@ -3,6 +3,10 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, './build')));
+console.log('1 Static files served from:', path.join(__dirname, './build'));
+console.log('2 Static files served from:', path.join(__dirname, '../build'));
+console.log('3 Static files served from:', __dirname);
+console.log('4 Static files served from:', path);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './build', 'index.html'));
